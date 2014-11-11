044a03eaf6d4e6eb9407a437ccfa317f120ba326
Wangda Zhang <zhangwangda@gmail.com>
2014-11-11 12:27:48 -0500

    add new coloring method

--- watermelonproject/src/watermelon/group6/Player.java
+++ watermelonproject/src/watermelon/group6/Player.java
@@ -57,16 +57,16 @@ public class Player extends watermelon.sim.Player {
 	public ArrayList<seed> topLeftPacking(ArrayList<Pair> trees, double width, double length, double s, boolean packColumns){
 
 		ArrayList<seed> seeds = new ArrayList<seed>();
-		
+
 		// swapped false means squish columns together.
 		// swapped true means squish rows together.
 		// Used to decide whether or not to flip the last row.
 		boolean secondToLastIsTetra = false;
 		double lastCoord = 0;
 		boolean lastIsTetra = false;
-		
+
 		boolean col = false;
-		
+
 		// Alternate coloring every other column (starting on left)
 		for (double i = distowall; i <= width - distowall; i += distBetweenSeeds) {
 			// Alternate coloring every other row (starting at top)
@@ -85,7 +85,7 @@ public class Player extends watermelon.sim.Player {
 					//Push the last column to the wall
 					if(i+distBetweenSeeds > width - distowall)
 						i = width - distowall;
-					
+
 					// pack columns
 					tmp = new seed(j, i, row);
 					secondToLastIsTetra = lastIsTetra;
@@ -96,11 +96,11 @@ public class Player extends watermelon.sim.Player {
 				}
 				else {
 					// pack rows
-					
+
 					//Push the last column to the wall
 					if(i+distBetweenSeeds > width - distowall)
 						i = width - distowall;
-					
+
 					tmp = new seed(i, j, row);
 					secondToLastIsTetra = lastIsTetra;
 					if (j > lastCoord) {
@@ -108,7 +108,7 @@ public class Player extends watermelon.sim.Player {
 					}
 					lastIsTetra = row;
 				}
-				
+
 				row = !row;
 				boolean add = true;
 				for (int f = 0; f < trees.size(); f++) {
@@ -123,7 +123,7 @@ public class Player extends watermelon.sim.Player {
 			}
 			col = !col;
 		}
-		
+
 		// change the last row to the opposite color
 		if (lastIsTetra == secondToLastIsTetra) {
 			if (packColumns) {
@@ -147,28 +147,28 @@ public class Player extends watermelon.sim.Player {
 				}
 			}
 		}
-		
+
 		System.out.printf("#seeds = %d\n", seeds.size());
-		return seeds;	
+		return seeds;
 	}
-	
+
 	//Pack starting top right
 	public ArrayList<seed> topRightPacking(ArrayList<Pair> trees, double width, double length, double s, boolean packColumns){
 
 		ArrayList<seed> seeds = new ArrayList<seed>();
-		
+
 		// swapped false means squish columns together.
 		// swapped true means squish rows together.
 		// Used to decide whether or not to flip the last row.
 		boolean secondToLastIsTetra = false;
 		double lastCoord = 0;
 		boolean lastIsTetra = false;
-		
+
 		boolean col = false;
-		
+
 		// Alternate coloring every other column (starting on right)
 		for (double i = width - distowall; i >= distowall; i -= distBetweenSeeds) {
-			
+
 			//Push the last row /column to the wall
 			// Alternate coloring every other row (starting at top)
 			boolean row = col;
@@ -183,18 +183,18 @@ public class Player extends watermelon.sim.Player {
 				// color it either diploid or tetraploid depending on the row.
 				// Save that information
 				if (packColumns) {
-					
+
 					//Push the last column to the wall
 					if(i-distBetweenSeeds < distowall)
 						i = distowall;
-					
+
 					// pack columns
 					tmp = new seed(j, i, row);
 					secondToLastIsTetra = lastIsTetra;
 					if (j > lastCoord) {
 						lastCoord = j;
 					}
-					lastIsTetra = col;					
+					lastIsTetra = col;
 				}
 				else {
 					//Push the last column to the wall
@@ -208,7 +208,7 @@ public class Player extends watermelon.sim.Player {
 					}
 					lastIsTetra = row;
 				}
-				
+
 				row = !row;
 				boolean add = true;
 				for (int f = 0; f < trees.size(); f++) {
@@ -223,7 +223,7 @@ public class Player extends watermelon.sim.Player {
 			}
 			col = !col;
 		}
-		
+
 		// change the last row to the opposite color
 		if (lastIsTetra == secondToLastIsTetra) {
 			if (packColumns) {
@@ -247,11 +247,11 @@ public class Player extends watermelon.sim.Player {
 				}
 			}
 		}
-		
+
 		System.out.printf("#seeds = %d\n", seeds.size());
-		return seeds;	
+		return seeds;
 	}
-	
+
 	// Pack from trees up to 10 layers
 	public ArrayList<seed> packFromTrees(ArrayList<Pair> trees, double width, double length, double s, ArrayList<seed> seedListNoTree) {
 		final double[] xoffset = {-1, 1, 2, 1, -1, -2};
@@ -294,11 +294,11 @@ public class Player extends watermelon.sim.Player {
 		System.out.printf("#seeds = %d\n", seeds.size());
 		return seeds;
 	}
-	
+
 	//Choose the packing which gives highest number of seeds
 	public ArrayList<seed> diagonal(ArrayList<Pair> trees, double width,
           double length, double s, boolean packColumns) {
-		
+
 		//Pack starting from top left
 		ArrayList<seed> seedListNoTreeLeft = topLeftPacking(trees, width, length, s, packColumns);
 		ArrayList<seed> seedListNoTreeRight = topRightPacking(trees, width, length, s, packColumns);
@@ -314,7 +314,7 @@ public class Player extends watermelon.sim.Player {
 
 		ArrayList<seed> seedListBruteLeft = fillGapsBrute(trees, width, length, s, seedListNoTreeLeft);
 		ArrayList<seed> seedListBruteRight = fillGapsBrute(trees, width, length, s, seedListNoTreeRight);
-		
+
 		//Calculate the score for each packing
 		List<Double> scores = new ArrayList<Double>();
 		List<ArrayList<seed>> seedLists = new ArrayList<ArrayList<seed>>();
@@ -326,14 +326,14 @@ public class Player extends watermelon.sim.Player {
 		seedLists.add(seedListBruteLeft);
 		scores.add(calculatescore(seedListBruteRight, s));
 		seedLists.add(seedListBruteRight);
-		
+
 		System.out.println("Top Left Packing Score: " + scores.get(0));
 		System.out.println("Top Right Packing Score: " + scores.get(1));
 		System.out.println("Top Left Brute Score: " + scores.get(2));
 		System.out.println("Top Right Brute Score: " + scores.get(3));
-		
+
 		double bestScore = Collections.max(scores);
-		
+
 		//Return the highest scoring packing
 		for (int i = 0; i < scores.size(); i++) {
 			if (scores.get(i) == bestScore) {
@@ -343,7 +343,7 @@ public class Player extends watermelon.sim.Player {
 		// Should never get here
 		return null;
 	}
-	
+
 	public ArrayList<seed> fillGapsBrute(ArrayList<Pair> trees, double width, double length,
 			double s, ArrayList<seed> seedListNoTrees) {
 		double i = 1;
@@ -357,11 +357,11 @@ public class Player extends watermelon.sim.Player {
 		}
 		return seedListNoTrees;
 	}
-	
+
 	public ArrayList<seed> tryPlace(double i, double j, ArrayList<seed> seedList, ArrayList<Pair> trees,
 			double width, double length, double s) {
 		seed newSeed = new seed(i, j, true);
-		System.out.println("trying new seed at " + i + ", " + j);
+		// System.out.println("trying new seed at " + i + ", " + j);
 		if (valid(newSeed, trees, seedList, width, length)) {
 			seedList.add(newSeed);
 			System.out.println("placed new seed at " + i + ", " + j);
@@ -387,20 +387,20 @@ public class Player extends watermelon.sim.Player {
           bestPacking = packRows;
           bestScore = scoreRows;
       }
-      
+
       // Start off calling recolor with the improvement equal to the score.
       // The improvement metric will get smaller as subsequent recolorings get closer to
       // the optimal.
       ArrayList<seed> recolored = recolor(bestPacking, bestScore, bestScore, s);
       return recolored;
   }
-	
+
 	public ArrayList<seed> recolor(ArrayList<seed> bestPacking, double bestScore,
 			double scoreImprovement, double sVal) {
-		if (scoreImprovement < .00001) {		
+		if (scoreImprovement < .00001) {
 		return bestPacking;
 		}
-		
+
 		/*// Find the best seed to change first.
 		seed bestSeedSoFar = new seed();
 		int bestSeedIndex = 0;
@@ -418,12 +418,12 @@ public class Player extends watermelon.sim.Player {
 				bestIndividScoreSoFar = newScore;
 			}
 		}
-		
+
 		// Start off bestPacking with the one best flipped seed
 		bestPacking.remove(bestSeedIndex);
 		bestPacking.add(bestSeedIndex, bestSeedSoFar);
 		*/
-		
+
 		// Create new board to test flips on. Only keeps the ones that make it better.
 		ArrayList<seed> flippedBoard = new ArrayList<>();
 		flippedBoard.addAll(bestPacking);
@@ -436,7 +436,7 @@ public class Player extends watermelon.sim.Player {
 			seed flipped = new seed(s.x, s.y, !s.tetraploid);
 			flippedBoard.add(bestPacking.indexOf(s), flipped);
 			flippedBoard.remove(s);
-			
+
 			// If flipping the seed gives a better overall score,
 			// keep it and update the score.
 			double newScore = calculatescore(flippedBoard, sVal);
@@ -472,7 +472,7 @@ public class Player extends watermelon.sim.Player {
 	// copied from simulator
 	double calculatescore(ArrayList<seed> seedlist, double s) {
 		double total = 0;
-		
+
 		for (int i = 0; i < seedlist.size(); i++) {
 			double score;
 			double chance = 0.0;
@@ -547,10 +547,14 @@ public class Player extends watermelon.sim.Player {
 
 	public ArrayList<seed> move(ArrayList<Pair> treelist, double width, double length, double s) {
 		startTime = System.currentTimeMillis();
-		ArrayList<seed> l1 = compact(treelist, width, length, s);
-		ArrayList<seed> l2 = ring(treelist, width, length, s);
-		ArrayList<seed> l3 = checkerboard(treelist, width, length, s);
-		
+		ArrayList<seed> l1_old = compact(treelist, width, length, s);
+		ArrayList<seed> l2_old = ring(treelist, width, length, s);
+		ArrayList<seed> l3_old = checkerboard(treelist, width, length, s);
+
+		ArrayList<seed> l1 = tryKernighanLin(l1_old, s);
+		ArrayList<seed> l2 = tryKernighanLin(l2_old, s);
+		ArrayList<seed> l3 = tryKernighanLin(l3_old, s);
+
 		double score1 = calculatescore(l1, s);
 		System.out.println("compact: " + score1);
 		double score2 = calculatescore(l2, s);
@@ -570,6 +574,185 @@ public class Player extends watermelon.sim.Player {
 		else {
 			return l3;
 		}
-		
+
+	}
+
+
+//////////////// Coloring: Kernighan-Lin for weighted graph bipartitioning
+	double calcScore(ArrayList<seed> seeds, double s) {
+		double total = 0;
+		int n = seeds.size();
+		double[] d = new double[n];
+
+		for (int i = 0; i < n; ++i) {
+			double t = 0;
+			for (int j = 0; j < n; ++j) {
+				if (j != i)
+					t = t + Math.pow(distance(seeds.get(i), seeds.get(j)), -2);
+			}
+			d[i] = 1/t;
+		}
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = i + 1; j < n; ++j) {
+				if (seeds.get(i).tetraploid == seeds.get(j).tetraploid)
+					continue;
+				total += (d[i] + d[j]) * Math.pow(distance(seeds.get(i), seeds.get(j)), -2);
+			}
+		}
+
+		return n * s + (1 - s) * total;
+	}
+
+	public ArrayList<seed> kernighanLin(ArrayList<seed> seedlist, double s) {
+		ArrayList<seed> seeds = new ArrayList<seed>();
+		for (seed sd : seedlist) {
+			seeds.add(new seed(sd.x, sd.y, sd.tetraploid));
+		}
+
+		double score_before = calcScore(seeds, s);
+		// System.out.println("Score before: " + score_before);
+
+		int n = seeds.size();
+		double[] dc = new double[n];
+		for (int i = 0; i < n; ++i) {
+			double t = 0;
+			for (int j = 0; j < n; ++j) {
+				if (j != i)
+					t = t + Math.pow(distance(seeds.get(i), seeds.get(j)), -2);
+			}
+			dc[i] = 1/t;
+		}
+		double[][] w = new double[n][n];
+		double wMax = 0.0;
+		for (int i = 0; i < n; ++i) {
+			for (int j = i + 1; j < n; ++j) {
+				w[i][j] = (dc[i] + dc[j]) * Math.pow(distance(seeds.get(i), seeds.get(j)), -2);
+				w[j][i] = w[i][j];
+				wMax = Math.max(wMax, w[i][j]);
+			}
+			w[i][i] = 0;
+		}
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				w[i][j] = wMax - w[i][j];
+			}
+		}
+
+		double gMax = 1.0;
+		while (gMax > 0) {
+			double[] d = new double[n];
+			int cnt1 = 0, cnt2 = 0;
+			for (int i = 0; i < n; ++i) {
+				d[i] = 0.0;
+				for (int j = 0; j < n; ++j) {
+					if (i == j) continue;
+					if (seeds.get(i).tetraploid == seeds.get(j).tetraploid)
+						d[i] -= w[i][j];
+					else
+						d[i] += w[i][j];
+				}
+				if (seeds.get(i).tetraploid)
+					cnt1 += 1;
+				else
+					cnt2 += 1;
+			}
+			if (cnt1 > cnt2) {
+				for (int i = 0; i < n; ++i)
+					seeds.get(i).tetraploid = !seeds.get(i).tetraploid;
+			}
+			int m = Math.min(cnt1, cnt2);
+			double[] g = new double[m];
+			int[] a = new int[m];
+			int[] b = new int[m];
+			boolean[] flag = new boolean[n];
+			for (int i = 0; i < n; ++i)
+				flag[i] = false;
+			for (int k = 0; k < m; ++k) {
+				g[k] = Double.NEGATIVE_INFINITY;
+				a[k] = -1;
+				b[k] = -1;
+				for (int i = 0; i < n; ++i) {
+					if (flag[i] || seeds.get(i).tetraploid == false)
+						continue;
+					for (int j = 0; j < n; ++j) {
+						if (flag[j] || seeds.get(j).tetraploid == true)
+							continue;
+						double cost = d[i] + d[j] - 2*w[i][j];
+						if (cost > g[k]) {
+							g[k] = cost;
+							a[k] = i;
+							b[k] = j;
+						}
+					}
+				}
+				//seeds.get(a[k]).tetraploid = false;
+				//seeds.get(b[k]).tetraploid = true;
+				flag[a[k]] = true;
+				flag[b[k]] = true;
+				for (int i = 0; i < n; ++i) {
+					if (flag[i]) continue;
+					d[i] = 0.0;
+					for (int j = 0; j < n; ++j) {
+						if (flag[j] || i == j) continue;
+						if (seeds.get(i).tetraploid == seeds.get(j).tetraploid)
+							d[i] -= w[i][j];
+						else
+							d[i] += w[i][j];
+					}
+				}
+			}
+			double tmp = 0;
+			int tmpk = -1;
+			gMax = Double.NEGATIVE_INFINITY;
+			for (int k = 0; k < m; ++k) {
+				tmp += g[k];
+				if (tmp > gMax) {
+					gMax = tmp;
+					tmpk = k;
+				}
+			}
+			if (gMax > 0) {
+				for (int i = 0; i <= tmpk; ++i) {
+					seeds.get(a[i]).tetraploid = !seeds.get(a[i]).tetraploid;
+					seeds.get(b[i]).tetraploid = !seeds.get(b[i]).tetraploid;
+				}
+			}
+		}
+
+		double score_after = calcScore(seeds, s);
+		// System.out.println("Score after: " + score_after);
+
+		if (score_after < score_before)
+			return seedlist;
+		return seeds;
+	}
+
+	public ArrayList<seed> tryKernighanLin(ArrayList<seed> seedlist, double s) {
+		ArrayList<seed> bestSeeds = new ArrayList<seed>();
+		for (seed sd : seedlist) {
+					bestSeeds.add(new seed(sd.x, sd.y, sd.tetraploid));
+		}
+		double bestScore = calcScore(seedlist, s);
+
+		for (int i = 0; i < seedlist.size(); ++i) {
+			ArrayList<seed> seeds = new ArrayList<seed>();
+			for (seed sd : seedlist) {
+				seeds.add(new seed(sd.x, sd.y, sd.tetraploid));
+			}
+			for (int j = 0; j <= i; ++j) {
+				seeds.get(j).tetraploid = false;
+			}
+			ArrayList<seed> tmpSeeds = kernighanLin(seeds, s);
+			double tmpScore = calcScore(tmpSeeds, s);
+			if (tmpScore > bestScore) {
+				bestSeeds.clear();
+				for (seed sd : tmpSeeds) {
+					bestSeeds.add(new seed(sd.x, sd.y, sd.tetraploid));
+				}
+				bestScore = tmpScore;
+			}
+		}
+		return bestSeeds;
 	}
 }
